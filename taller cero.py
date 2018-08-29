88c897f1ce6abd0fea7101ac04898b323bf3ef98
Unknown <sir.damm@gmail.com>
2018-08-29 16:18:12 -0300

    Actualizacion del ej 4

--- taller cero.py
+++ taller cero.py
@@ -31,24 +31,30 @@ def jugarVarios(a,n): # a: es una lista que representa un mazo  n: numero de jug
         lista_total[i] = jugar(a)  # cada jugador "juega" y se guarda su total en la posicion i-esima
     return lista_total             # la funcion regresa una lista con el total de cada jugador.
 
-test = [32,32,32]
-
+ 
 def verQuienGano(res):  #res = lista de los resultados. Se considera el ganador al primer jugador (en orden) en estar mas cerca de 21.
-     QuienGano = False      #posicion del personaje que gano. None si no gano nadie.
-     puntaje_minimo = 21   # Busco un jugador con este puntaje, si no hay nadie lo incremento en 1.
-     puntaje_maximo = 32   # el maximo puntaje posible es 20+12. Nadie gana si todos llegaron a 32.
-     
-     while QuienGano == False:   # el loop continua hasta que alguien gana o se determina que nadie gana (int o None).
-         if puntaje_minimo != puntaje_maximo:  # para que se corte cuando min = max
-             for jugador in range(len(res)):   # Busca sobre los resultados si algun jugador tiene el puntaje minimo
-                 if res[jugador] == puntaje_minimo and QuienGano == False: # esta condicion esta para que gane el primer jugador en llegar al minimo (el de menor i)
-                     QuienGano = jugador
-             puntaje_minimo += 1  # si nadie tiene 21, se aumenta la cuenta en 1 y se repite hasta que puntaje_min = puntaje_max
-         else: QuienGano = None  # si puntaje_min = puntaje_max, entonces nadie gano (todos en 32)
-     return QuienGano
-            
+    QuienGano = None    #posicion del personaje que gano
+    puntaje_minimo = 0   # Busco el primer jugador con este puntaje, si no hay nadie lo incremento en 1.
+    res_dist = [abs(x - 21) for x in res]  # distancia al 21, para buscar ganadores mas facilmente.
+    while QuienGano == None :    # el loop continua hasta que se encuentra un ganador, siempre hay uno, incluso en caso de empate ya que gana el primero.
+        i=0
+        for jugador in res_dist:
+            if jugador == puntaje_minimo and QuienGano == None:
+                QuienGano = i
+            else:
+                i+=1
+        puntaje_minimo += 1
+    return QuienGano
 
-                 
+def jugar(a):  # a: es una lista que representa un mazo
+    total=0     # inicializo la suma del jugador
+    while total < 21:  # loop que se para cuando el total es >= a 21
+        total += a.pop()  #elimina el primer item del mazo y lo suma al total
+    return total   #la funcion regresa la suma del jugador.
+
+test = [19,26,20,15,21]
+
+print(verQuienGano(test))
          
              
          
